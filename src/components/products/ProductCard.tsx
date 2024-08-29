c08aaecc54e8cfec2f7974dff058d3f88d636ef0
suyunovdev <153995037+suyunovdev@users.noreply.github.com>
2024-08-30 01:57:03 +0500

    finish lesson

---
+++ src/components/products/ProductCard.tsx
@@ -0,0 +1,19 @@
+import { ProductType } from "@/types/product.types";
+
+type ProductCardProps = {
+  product: ProductType;
+};
+
+const ProductCard = ({ product }: ProductCardProps) => {
+  return (
+    <div className="p-5 basis-[30%] shadow-md rounded-lg">
+      <img
+        src={product.image}
+        alt={product.title}
+        className="w-full h-[300px] object-contain"
+      />
+      {product.id}. {product.title}
+    </div>
+  );
+};
+export default ProductCard;
